9fcaf9e38cb111774b15129e0a8b1c0f721f82d2
Denuk68 <denus.denisovuch@gmail.com>
2022-11-05 10:12:40 +0200

    lesson5_classwork

--- src/components/UpdateUsers.js
+++ src/components/UpdateUsers.js
@@ -16,10 +16,19 @@ export default function UpdateUsers() {
     }
 
     const [user, setUser] = useState({});
-
     const getUserOnSubmit = (e) => {
         e.preventDefault();
-        getUser(userId).then(data => console.log(data))
+        getUser(userId).then(data => setUser({ ...data }))
+    }
+
+    // Update user form
+    const [formState, setFormState] = useState({ id: "", name: "", email: "" });
+    const onFormInputChange = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    const onSubmitUpdateForm = (e) => {
+        e.preventDefault();
+        console.log(formState)
     }
 
     return (
@@ -37,9 +46,10 @@ export default function UpdateUsers() {
             <hr />
 
             <p>Update user:</p>
-            <form>
-                <input type={"text"} name={"name"} placeholder={"name"} />
-                <input type={"email"} name={"email"} placeholder={"email"} />
+            <form onSubmit={onSubmitUpdateForm}>
+                <input type={"number"} name={"id"} placeholder={"id"} onInput={onFormInputChange} />
+                <input type={"text"} name={"name"} placeholder={"name"} onInput={onFormInputChange} />
+                <input type={"email"} name={"email"} placeholder={"email"} onInput={onFormInputChange} />
                 <button>Update</button>
             </form>
 
