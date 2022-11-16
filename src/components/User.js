409a2bb6dd51e38118b7e97b792abf598da99a05
Denuk68 <denus.denisovuch@gmail.com>
2022-11-16 19:18:40 +0200

    lesson6

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export default function User({ user }) {
     return (
         <div>
-            {user.name}-<Link to={{ pathname: `${user.id}`, state: user }}>user details</Link>
+            {user.name}-<Link to={"/users/" + user.id} state={{ user }}>user details</Link>
         </div >
 
     )
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,14 @@
+import { useLocation } from "react-router"
+
 export default function UsersDetails() {
+    let location = useLocation();
+    // let user = location.state.user  Не деструкторизований варіант =)
+    let { state: { user } } = location;
 
     return (
         <div>
-            <h2>UsersDetails</h2>        
+            <h2>UsersDetails</h2>
+            {JSON.stringify(user)}
         </div>
     )
 }
