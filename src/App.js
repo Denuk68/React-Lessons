ead36c4d7c41282e3694972c5b746b83da474b2f
Denuk68 <denus.denisovuch@gmail.com>
2022-11-18 13:12:21 +0200

    lesson7 part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useReducer } from "react";
+import reducer from "./reducers/reducer";
+
+
+
 export default function App() {
+
+  let [{ a, b }, dispatch] = useReducer(reducer, { a: 0, b: 0 });
+  // state = {a:0, b:0}
+
+
   return (
-    <h1>
-      Hello world
-    </h1>
+    <div>
+      <h1>state1 : {a}</h1>
+      <button onClick={() => dispatch({ obj: '1', diya: "+" })}>inc</button>
+      <button onClick={() => dispatch({ obj: '1', diya: "-" })}>dec</button>
+      <h1>state2 : {b}</h1>
+      <button onClick={() => dispatch({ obj: '2', diya: "+" })}>inc</button>
+      <button onClick={() => dispatch({ obj: '2', diya: "-" })}>dec</button>
+    </div>
   );
-}
+} 
