b1d4af637bd10e43a00984710147fff138af6c89
Denuk68 <denus.denisovuch@gmail.com>
2022-11-22 20:29:40 +0200

    lesson7_homework

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+   if(action.diya === "+10"){
+    return {...state, number: state.number +10 }
+   }
+   else if(action.diya === "-2"){
+    return {...state, number: state.number-2}
+   }
+   else{
+    return {...state}
+   }
+ }
+
 export default function App() {
+  
+  const[{number} , dispatch]= useReducer(reducer, {number: 0})
+  
   return (
     <div>
-      App
+      number: {number}
+      <br/>
+      <button onClick={() => {dispatch({diya: '+10'})}}>+ 10</button>
+      <button onClick={() => {dispatch({diya: '-2'})}}>- 2</button>
     </div>
   );
 }
