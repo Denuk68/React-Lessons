7b5ca3591a1d518f9b4cc3668c3e27d3f18da639
Denuk68 <denus.denisovuch@gmail.com>
2023-01-03 15:33:27 +0200

    lesson8_redux1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import { Provider } from "react-redux"
+
+
+let initialState = { user: [] };
+const rootReducer = (state = initialState, action) => {
+  console.log(state);
+  return state;
+}
+export let store = crateStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
