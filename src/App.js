6db38bd0d3cc78477f3ea378637126d5f781aa3d
Denuk68 <denus.denisovuch@gmail.com>
2023-01-11 17:40:22 +0200

    lesson8_redux1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "./services/users.api";
+import { addUser, fetchUsers } from "./services/users.api";
 
 export default function App() {
   let state = useSelector(state => state);
@@ -18,7 +18,13 @@ export default function App() {
 
   let onSubmit = (e) => {
     e.preventDefault();
-
+    let name = e.target.name.value;
+    let user = { name }
+    // todo send request
+    addUser(user).then(value => {
+      console.log('saved users ->', value);
+      dispatch({type:'PUSH_USERS', payload: value})
+    })
   }
 
 
@@ -26,10 +32,12 @@ export default function App() {
   return (
     <div>
       <form onSubmit={onSubmit}>
+        <input type="text" name={"name"}></input>
+        <button>add user</button>
 
       </form>
       <hr />
-      
+
       {users.map((value, index) => <div key={index}>{value.name}</div>)}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ import { Provider } from "react-redux"
 
 
 const rootReducer = (state = { users: [] }, action) => {
+  // console.log('reducer', state)
+  console.log(action)
   switch (action.type) {
     case "FETCH_USERS":
-      
       return { ...state, users: [...action.payload] };
     case "CLEAR_STORE":
       return { ...state, users: [] }
+    case "PUSH_USERS":
+      console.log(action.payload)
+      return { ...state, users: [...state.users, action.payload] }
     default: return state;
   }
 }
